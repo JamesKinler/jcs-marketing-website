de847cf5fe66586fb2f7f3e2583562ba2815652c
Office <Office@Office-iMac.local>
2018-09-07 12:40:21 -0700

    wrapping some of js in a if statment

--- js/myjs.js
+++ js/myjs.js
@@ -36,67 +36,76 @@ var ParallaxScene = new ScrollMagic.Scene({
 
 
 
+if($('#counter1' && '#counter2' && '#counter').length > 0){
 //this scene counts to 3
-var Cont={val:0} , NewVal = 3 ;
+  var Cont={val:0} , NewVal = 3 ;
 
-var Tween1 = TweenLite.to(Cont,3,{val:NewVal,roundProps:"val",onUpdate:function(){
-  document.getElementById("counter1").innerHTML=Cont.val
-}});
+  var Tween1 = TweenLite.to(Cont,3,{val:NewVal,roundProps:"val",onUpdate:function(){
+    document.getElementById("counter1").innerHTML=Cont.val
+  }});
 
-var scene1 = new ScrollMagic.Scene({
- triggerElement: '.we_number',
- triggerHook: 1,
- reverse: false,
-})
-.setTween(Tween1)
-.addTo(controller);
-
-
-
-// this scene counts to 6
-var Counter={val:0} , NewValue = 6 ;
-var Tween2 = TweenLite.to(Counter,3,{val:NewValue,roundProps:"val",onUpdate:function(){
-  document.getElementById("counter2").innerHTML=Counter.val
-}});
-
-var scene1 = new ScrollMagic.Scene({
- triggerElement: '.we_number',
- triggerHook: 1,
- reverse: false,
-})
-.setTween(Tween2)
-.addTo(controller);
+  var scene1 = new ScrollMagic.Scene({
+   triggerElement: '.we_number',
+   triggerHook: 1,
+   reverse: false,
+  })
+  .setTween(Tween1)
+  .addTo(controller);
 
 
 
-//this adds a comma or decimal to scene 4
-Number.prototype.numberFormat = function(decimals, dec_point, thousands_sep) {
-    dec_point = typeof dec_point !== 'undefined' ? dec_point : '.';
-    thousands_sep = typeof thousands_sep !== 'undefined' ? thousands_sep : ',';
+  // this scene counts to 6
+  var Counter={val:0} , NewValue = 6 ;
+  var Tween2 = TweenLite.to(Counter,3,{val:NewValue,roundProps:"val",onUpdate:function(){
+    document.getElementById("counter2").innerHTML=Counter.val
+  }});
 
-    var parts = this.toFixed(decimals).split('.');
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
+  var scene1 = new ScrollMagic.Scene({
+   triggerElement: '.we_number',
+   triggerHook: 1,
+   reverse: false,
+  })
+  .setTween(Tween2)
+  .addTo(controller);
 
-    return parts.join(dec_point);
+  //this adds a comma or decimal to scene 4
+  Number.prototype.numberFormat = function(decimals, dec_point, thousands_sep) {
+      dec_point = typeof dec_point !== 'undefined' ? dec_point : '.';
+      thousands_sep = typeof thousands_sep !== 'undefined' ? thousands_sep : ',';
+
+      var parts = this.toFixed(decimals).split('.');
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
+
+      return parts.join(dec_point);
+  }
+  var counter = { var: 0 };
+  var tal = document.getElementById("counter");
+   var Tween = TweenMax.to(counter, 5, {
+        var: 20000,
+        onUpdate: function () {
+            var nwc = counter.var.numberFormat(0);
+            tal.innerHTML = nwc;
+        },
+        ease:Circ.easeOut
+    });
+  // this scene is the numbers to scroll to 20000
+  var scene4 = new ScrollMagic.Scene({
+    triggerElement: '.we_number',
+    triggerHook: 1,
+    reverse: false,
+  })
+  .setTween(Tween)
+  .addTo(controller);
+
+
 }
-var counter = { var: 0 };
-var tal = document.getElementById("counter");
- var Tween = TweenMax.to(counter, 5, {
-      var: 20000,
-      onUpdate: function () {
-          var nwc = counter.var.numberFormat(0);
-          tal.innerHTML = nwc;
-      },
-      ease:Circ.easeOut
-  });
-// this scene is the numbers to scroll to 20000
-var scene4 = new ScrollMagic.Scene({
-  triggerElement: '.we_number',
-  triggerHook: 1,
-  reverse: false,
-})
-.setTween(Tween)
-.addTo(controller);
+
+
+
+
+
+
+
 
 
 
