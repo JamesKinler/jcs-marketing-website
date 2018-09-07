31982713cf96e9bcc98b8c87612e6464712bb120
Office <Office@Office-iMac.local>
2018-09-07 12:16:51 -0700

    adding the services page to the website

--- js/myjs.js
+++ js/myjs.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   // this has the scrolling text at the top of the hero image
   var typed = new Typed('.element', {
-    strings: ["Marketing Needs. ^1000", "Trade Show Needs. ^1000", "Web Development Needs. ^1000"],
+    strings: ["Marketing Needs. ^1000", "Web And Design Needs. ^1000"],
     typeSpeed: 30,
     loop: true,
     loopCount: 5,
@@ -110,6 +110,22 @@ var fadeInLeft = new ScrollMagic.Scene({
 })
 .setClassToggle('.services','fadeInLeft')
 .addTo(controller);
+
+//hamburger navbar
+
+function resizeNav() {
+    $(".menu").css({"height": window.innerHeight});
+    var radius = Math.sqrt(Math.pow(window.innerHeight, 2) + Math.pow(window.innerWidth, 2));
+    var diameter = radius * 2;
+    $(".nav-layer").width(diameter);
+    $(".nav-layer").height(diameter);
+    $(".nav-layer").css({"margin-top": -radius, "margin-left": -radius});
+}
+$(".nav-toggle").click(function() {
+    $(".nav-toggle, .nav-layer, .menu").toggleClass("open");
+});
+$(window).resize(resizeNav);
+resizeNav();
 
 
 });
